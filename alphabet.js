50a45adcc73adea0254f008126fc32edcd67cdba
Pere Pages <pere.pages@ocado.com>
2019-08-19 18:13:49 +0200

    refactor: structure

--- alphabet.js
+++ alphabet.js
@@ -27,31 +27,13 @@ const letters = [
   }
 ];
 
-const inverse = [
-  {
-    ".-": pattern("a")
-  },
-  {
-    "-...": pattern("b")
-  },
-  {
-    "-.-.": pattern("c")
-  },
-  {
-    "-..": pattern("d")
-  },
-  {
-    ".": pattern("e")
-  },
-  {
-    "..-.": pattern("f")
-  },
-  {
-    "--.": pattern("g")
-  },
-  {
-    "....": pattern("h")
-  }
-];
+function getInverse() {
+  const res = [];
+  letters.forEach(letter => {
+    const index = Object.keys(letter)[0];
+    res.push({ [letter[index].pattern]: pattern(index) });
+  });
+  return res;
+}
 
-module.exports = { letters, inverse };
+module.exports = letters.concat(getInverse());
--- main.js
+++ main.js
@@ -1,43 +1,21 @@
 const prompt = require("prompt");
 
-const { letters, inverse } = require("./alphabet");
+const shuffle = require("./shuffle");
+const morseSet = require("./alphabet");
 
-const mixed = [...letters, ...inverse];
+const main = () => {
+  shuffle(morseSet);
 
-shuffle(mixed);
+  const schema = {
+    properties: {}
+  };
 
-const schema = {
-  properties: {}
+  morseSet.forEach(l => (schema.properties = { ...schema.properties, ...l }));
+
+  prompt.start();
+  prompt.get(schema, (_, res) => {
+    console.log("Keep practicing!");
+  });
 };
 
-mixed.forEach(l => (schema.properties = { ...schema.properties, ...l }));
-
-prompt.start();
-prompt.get(schema, (_, res) => {
-  console.log("Keep practicing!");
-});
-
-/**
- * Randomly shuffle an array
- * https://stackoverflow.com/a/2450976/1293256
- * @param  {Array} array The array to shuffle
- * @return {String}      The first item in the shuffled array
- */
-function shuffle(array) {
-  var currentIndex = array.length;
-  var temporaryValue, randomIndex;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
+main();
